bed3be2690a8ad10f661f2a56f0e6934d9637aef
Travis Person <travis.person@gmail.com>
2015-06-03 16:44:53 -0700

    Initial

---
+++ test.js
@@ -0,0 +1,15 @@
+var test = require('tape')
+var abstractBlobTests = require('abstract-blob-store/tests')
+var blobs = require('./')
+
+var common = {
+  setup: function (t, cb) {
+    var store = blobs()
+    cb(null, store)
+  },
+  teardown: function (t, store, blob, cb) {
+    cb()
+  }
+}
+
+abstractBlobTests(test, common)
