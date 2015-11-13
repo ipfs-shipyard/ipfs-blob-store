e73309ba3f08980a3ec65f85975267f2b0ad5646
David Dias <daviddias.p@gmail.com>
2015-11-13 14:48:41 +0100

    remove default basedir

--- tests/mfs.js
+++ tests/mfs.js
@@ -5,7 +5,15 @@ var ipfsBlobStore = require('../index.js')
 var common = {
   setup: function (t, cb) {
     var store = ipfsBlobStore().mfs
-    cb(null, store)
+
+    store.baseDir = '/tests/'
+
+    store.node.files.mkdir(store.baseDir, { p: true }, function (err) {
+      if (err) {
+        return console.error(err)
+      }
+      cb(null, store)
+    })
   },
   teardown: function (t, store, blob, cb) {
     store.node.files.rm(store.baseDir, { 'r': true }, function (err) {
