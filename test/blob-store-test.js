b2e6e8f38c8a1df7e67dae6ef92444b30d9d5fe1
achingbrain <alex@achingbrain.net>
2018-08-13 12:08:21 +0100

    fix: use bundled ipfs in tests

--- test/blob-store-test.js
+++ test/blob-store-test.js
@@ -7,21 +7,36 @@ const DaemonFactory = require('ipfsd-ctl')
 const df = DaemonFactory.create()
 const which = require('which')
 
-which('ipfs', (error, ipfs) => {
+which('jsipfs', (error, ipfs) => {
   if (error) {
     throw error
   }
 
   df.spawn({
     disposable: true,
-    args: ['--api=/ip4/127.0.0.1/tcp/13000'],
     exec: ipfs
-  }, (err, node) => {
+  }, async (err, node) => {
     if (err) {
       throw err
     }
 
-    abstractBlobTests(test, common)
+    const options = {
+      baseDir: '/tests/',
+      ipfs: node.api
+    }
+    const store = await ipfsBlobStore(options)
+
+    abstractBlobTests(test, {
+      setup: async (t, cb) => {
+        await store.ipfs.files.mkdir(options.baseDir, { p: true })
+        cb(null, store)
+      },
+      teardown: async (t, store, blob, cb) => {
+        await store.ipfs.files.rm(options.baseDir, { recursive: true })
+        cb()
+      }
+    })
+
     // quick hack to stop the deamon
     // TODO clean up later
     setTimeout(function () {
@@ -29,22 +44,3 @@ which('ipfs', (error, ipfs) => {
     }, 5000)
   })
 })
-
-let store
-const options = {
-  baseDir: '/tests/',
-  port: 13000,
-  host: '127.0.0.1'
-}
-const common = {
-  setup: async (t, cb) => {
-    store = await ipfsBlobStore(options)
-    await store.ipfs.files.mkdir(options.baseDir, { p: true })
-    cb(null, store)
-  },
-
-  teardown: async (t, store, blob, cb) => {
-    await store.ipfs.files.rm(options.baseDir, { recursive: true })
-    cb()
-  }
-}
