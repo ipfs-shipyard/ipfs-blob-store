9f06ade2ddc3704fb0c9cdfb74253c75c3ad258c
Nitin Patel <nitinpatel278@gmail.com>
2019-03-22 18:50:48 +0530

    docs: fix example

--- examples/mfs-example.js
+++ examples/mfs-example.js
@@ -1,16 +1,16 @@
 'use strict'
 
-var ipfsBlobStore = require('../index.js')
-var store = ipfsBlobStore().mfs
+var ipfsBlobStore = require('../src')
 
-/*
-process.stdin.pipe(store.createWriteStream('test/2/3/4', function (err, metadata) {
-  if (err) {
-    // console.log(err)
-  }
+ipfsBlobStore().then(store => {
+  /*
+  process.stdin.pipe(store.createWriteStream('test/2/3/4', function (err, metadata) {
+    if (err) {
+      // console.log(err)
+    }
 
-  console.log(metadata)
-}))
-*/
-
-store.createReadStream('test/2/3/4').pipe(process.stdout)
+    console.log(metadata)
+  }))
+  */
+  store.createReadStream('test/2/3/4').pipe(process.stdout)
+})
